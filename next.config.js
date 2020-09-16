78ef02778d7a5703dd81bdc968d55b4083fc38ea
Steven Keezer <stevenkeezer@Stevens-MacBook.local>
2020-09-16 13:56:21 -0700

    changed ntex.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
+const withPlugins = require("next-compose-plugins");
 const withCSS = require("@zeit/next-css");
+const withBabelMinify = require("next-babel-minify");
+const withProgressBar = require("next-progressbar");
 
-module.exports = withCSS();
+const nextConfigs = {
+  webpack: (config) => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: "empty",
+    };
+    config.plugins = [...config.plugins];
+    return config;
+  },
+  target: "serverless",
+};
+
+module.exports = withPlugins(
+  [withCSS({}), withBabelMinify(), withProgressBar({})],
+  nextConfigs
+);
